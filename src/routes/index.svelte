7b97d26551477c0c47b0c3c5f50958b4c40facbd
draco696 <dagixmeko@gmail.com>
2022-03-28 17:15:53 +0300

    coming soon message 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,13 @@
-<!-- <div class=" w-full grid place-items-center h-screen home">
-	<div class="w-full sm:h-auto h-landingH1 relative z-0">
-		<img src="./src/assets/logo/amdlogo.png" alt="AMD logo" />
-		<h1 class="text-6xl text-white">Coming soon</h1>
-	</div>
-</div> -->
 <div class="w-full">
 	<div class="w-full h-screen relative z-0">
 		<img
-			src="./src/assets/images/landing.jpg"
+			src="./static/assets/images/landing.jpg"
 			class="object-cover h-screen"
 			alt="Landing background"
 		/>
 		<div class="absolute inset-0 grid z-10 bg-black bg-opacity-80 ">
 			<div class="grid place-items-center h-screen">
-				<img src="./src/assets/logo/amdlogo.png" alt="AMD logo" />
+				<img src="./static/assets/logo/amdlogo.png" alt="AMD logo" />
 				<h1 class="text-5xl text-white">Welcome to AMD Import and Export!</h1>
 				<h1 class="text-4xl text-white">Our website is under construction!</h1>
 			</div>
