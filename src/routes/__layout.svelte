5c82354887b23cbe37b8d77d6646a1a7b33d35a4
draco696 <dagixmeko@gmail.com>
2022-03-28 16:04:36 +0300

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '$lib/styles/app.css';
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+<slot />
+
+<footer>
+	<p>AMD international Trading</p>
+</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl font-bold underline bg-red-500 justify-center ">Hello world!</h1>
