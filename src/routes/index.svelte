2976949da53b94f629e0840ef1c4cef3777b847d
draco696 <dagixmeko@gmail.com>
2022-03-28 18:53:41 +0300

    coming soon message 4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,18 @@
+<script>
+	import landing from '../../static/assets/images/landing.jpg';
+	import logo from '../../static/assets/logo/amdlogo.png';
+</script>
+
 <div class="w-full">
 	<div class="w-full h-screen relative z-0">
-		<img
-			src="./assets/images/landing.jpg"
-			class="object-cover h-screen w-full"
-			alt="Landing background"
-		/>
+		<img src={landing} class="object-cover h-screen w-full" alt="Landing background" />
 		<div class="absolute inset-0 grid z-10 bg-black bg-opacity-80 ">
 			<div class="grid place-items-center h-screen">
-				<img src="./assets/logo/amdlogo.png" alt="AMD logo" />
-				<h1 class="text-5xl text-white text-center ">Welcome to AMD Import and Export!</h1>
-				<h1 class="text-4xl text-white">Our website is under construction!</h1>
+				<img src={logo} alt="AMD Logo" />
+				<h1 class="lg:text-5xl text-3xl text-white text-center ">
+					Welcome to AMD Import and Export!
+				</h1>
+				<h1 class="lg:text-4xl text-xl text-white">Our website is under construction!</h1>
 			</div>
 		</div>
 	</div>
