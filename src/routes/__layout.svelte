9c14f680f76b95681cef627fe6944a980b1b550f
draco696 <dagixmeko@gmail.com>
2022-03-28 17:03:34 +0300

    coming soon message

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,14 +2,4 @@
 	import '$lib/styles/app.css';
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
-</nav>
-
 <slot />
-
-<footer>
-	<p>AMD international Trading</p>
-</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,29 @@
-<h1 class="text-3xl font-bold underline bg-red-500 justify-center ">Hello world!</h1>
+<!-- <div class=" w-full grid place-items-center h-screen home">
+	<div class="w-full sm:h-auto h-landingH1 relative z-0">
+		<img src="./src/assets/logo/amdlogo.png" alt="AMD logo" />
+		<h1 class="text-6xl text-white">Coming soon</h1>
+	</div>
+</div> -->
+<div class="w-full">
+	<div class="w-full h-screen relative z-0">
+		<img
+			src="./src/assets/images/landing.jpg"
+			class="object-cover h-screen"
+			alt="Landing background"
+		/>
+		<div class="absolute inset-0 grid z-10 bg-black bg-opacity-80 ">
+			<div class="grid place-items-center h-screen">
+				<img src="./src/assets/logo/amdlogo.png" alt="AMD logo" />
+				<h1 class="text-5xl text-white">Welcome to AMD Import and Export!</h1>
+				<h1 class="text-4xl text-white">Our website is under construction!</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-family: 'Open Sans';
+		font-style: normal;
+	}
+</style>
